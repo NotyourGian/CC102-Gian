7cc1870e8a8724cc57d28bdd9484191788777bc6
NotyourGian <trinanesgian@gmail.com>
2024-10-03 10:10:38 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+
+{
+	string Nickname;
+	string Course;
+	string School;
+	
+	cout << "Enter your Nickname\t\t\t:" << endl;
+	cin >> Nickname;
+	cout << "Enter your Course\t\t\t:" << endl;
+	cin >> Course;
+	cout << "Enter your School\t\t\t:" << endl;
+	cin >> School;
+	cout << "******************************************************" << endl;
+	cout << "WoW Congrats" << endl;
+	return 0;
+	
+	
+}
