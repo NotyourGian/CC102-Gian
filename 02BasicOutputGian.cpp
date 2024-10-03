c450c37c86f01296b73b1b7f7c982c8b8aaf85d0
NotyourGian <trinanesgian@gmail.com>
2024-10-03 10:10:09 +0800

    Add files via upload

--- 02BasicOutputGian.cpp
+++ 02BasicOutputGian.cpp
@@ -7,7 +7,7 @@ int main()
 
 {
 	cout << "SCHOOL\t\t\t:\tOLLCF" << endl;
-	cout << "DEPARTMENT\t\t:\tCCS" << endl;
+	cout << "DEPARTMENT\t\t:\tBSIT" << endl;
 	cout << "COURSE & YEAR LEVEL\t:       BSIT 1st year" << endl;
 	cout << "NAME\t\t\t:\tGian Christoffe M Trinanes" << endl;
 	cout << "ADDRESS\t\t\t:\tBrgy. Tanawan Capalonga Cam. Norte " << endl;
