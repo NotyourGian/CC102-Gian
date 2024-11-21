cc1e4fa7c774f55432416fe7d2d503629fee7701
NotyourGian <trinanesgian@gmail.com>
2024-11-21 20:10:48 +0800

    Add files via upload

---
+++ GianActivity2_ElseIfOptimizatin.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	int age;
+	cout << "Enter your age: ";
+	cin >> age;
+	if (age<16)
+		cout << "Too young to drive";
+	else if (age==16) 
+		cout << "Better clear the road.";
+	else 
+		cout << "You are getting pretty old.";
+	return 0;
+}
